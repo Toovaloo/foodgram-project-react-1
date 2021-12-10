10de817cae71edd2a36e769ca067ebd65066e7bd
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2021-12-11 00:59:05 +1000

    Implement the register a new user test

--- tests/test_00_users.py
+++ tests/test_00_users.py
@@ -10,7 +10,7 @@ class Test00Users:
     url = '/api/users/'
 
     @pytest.mark.django_db(transaction=True)
-    def test_01_users_not_authenticated(self, client):
+    def test_00_users_not_authenticated(self, client):
         response = client.get(self.url)
 
         assert response.status_code != 404, (
@@ -22,7 +22,7 @@ class Test00Users:
         )
 
     @pytest.mark.django_db(transaction=True)
-    def test_02_users_authenticated(self, auth_user_client, user):
+    def test_00_users_authenticated(self, auth_user_client, user):
         response = auth_user_client.get(self.url)
 
         assert response.status_code != 404, (
@@ -51,7 +51,7 @@ class Test00Users:
         )
 
     @pytest.mark.django_db(transaction=True)
-    def test_03_nodata_signup(self, client):
+    def test_00_nodata_signup(self, client):
         response = client.post(self.url)
 
         assert response.status_code != 404, (
@@ -72,3 +72,97 @@ class Test00Users:
                 f'Проверьте, что при {request_type} запросе `{self.url}` без параметров '
                 f'в ответе есть сообщение о том, какие поля заполенены неправильно'
             )
+
+    @pytest.mark.django_db(transaction=True)
+    def test_00_invalid_data_signup(self, client):
+        invalid_email = 'invalid_email'
+        invalid_username = 'invalid username'
+
+        invalid_data = {
+            'email': invalid_email,
+            'username': invalid_username
+        }
+        request_type = 'POST'
+        response = client.post(self.url, data=invalid_data)
+
+        assert response.status_code != 404, (
+            f'Страница `{self.url}` не найдена, проверьте этот адрес в *urls.py*'
+        )
+        code = 400
+        assert response.status_code == code, (
+            f'Проверьте, что при {request_type} запросе `{self.url}` с невалидными данными '
+            f'не создается пользователь и возвращается статус {code}'
+        )
+
+        response_json = response.json()
+        invalid_fields = ['email', 'username']
+        for field in invalid_fields:
+            assert (field in response_json.keys()
+                    and isinstance(response_json[field], list)), (
+                f'Проверьте, что при {request_type} запросе `{self.url}` с невалидными параметрами, '
+                f'в ответе есть сообщение о том, какие поля заполенены неправильно'
+            )
+
+        valid_email = 'validemail@yamdb.fake'
+        invalid_data = {
+            'email': valid_email,
+        }
+        response = client.post(self.url, data=invalid_data)
+        assert response.status_code == code, (
+            f'Проверьте, что при {request_type} запросе `{self.url}` без username '
+            f'нельзя создать пользователя и возвращается статус {code}'
+        )
+
+    @pytest.mark.django_db(transaction=True)
+    def test_00_valid_data_user_signup(self, client):
+
+        valid_email = 'valid@yamdb.fake'
+        valid_username = 'valid_username'
+        valid_first_name = 'Ivan'
+        valid_last_name = 'Ivanov'
+        valid_password = 'sECretPaSSw0rd'
+
+        valid_data = {
+            'email': valid_email,
+            'username': valid_username,
+            'first_name': valid_first_name,
+            'last_name': valid_last_name,
+            'password': valid_password,
+        }
+        request_type = 'POST'
+        response = client.post(self.url, data=valid_data)
+
+        assert response.status_code != 404, (
+            f'Страница `{self.url}` не найдена, проверьте этот адрес в *urls.py*'
+        )
+
+        code = 201
+        assert response.status_code == code, (
+            f'Проверьте, что при {request_type} запросе `{self.url}` с валидными данными '
+            f'создается пользователь и возвращается статус {code}'
+        )
+        data = response.json()
+        assert (
+            data.get('username') == valid_username
+            and data.get('email') == valid_email
+            and data.get('first_name') == valid_first_name
+            and data.get('last_name') == valid_last_name
+        ), (
+            f'Проверьте, что при {request_type} запросе `{self.url}` с валидными данными '
+            f'создается пользователь и возвращается статус {code}'
+        )
+
+        hidden_fields = ['password', ]
+        for field in hidden_fields:
+            assert field not in data.keys(), (
+                f'Проверьте, что при {request_type} запросе `{self.url}`'
+                f'возвращяются только предусмотренные спецификацией поля.'
+            )
+
+        new_user = User.objects.filter(email=valid_email)
+        assert new_user.exists(), (
+            f'Проверьте, что при {request_type} запросе `{self.url}` с валидными данными '
+            f'создается пользователь и возвращается статус {code}'
+        )
+
+        new_user.delete()
